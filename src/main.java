77aaa9d35a2ee1cf01d81b1ee5d3527845816fa6
Thomas Rostrup Andersen <thomas@dhcp-070111.wlan.ntnu.no>
2016-03-15 11:48:03 +0100

    integreated textUI.

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.TextUI;
 import java.sql.*;
 
 /**
@@ -17,21 +18,25 @@ public class main {
         Date date_achieved = new Date(115, 03, 21);
         Time time = new Time(1,1,3);
 
+
         DBIN dbin = new DBIN(dbURL, username, password);
-        dbin.insertEntry(date_made, time, 100);
-        dbin.insertTemplate("m1");
+        //dbin.insertEntry(date_made, time, 100);
+        //dbin.insertTemplate("m1");
         dbin.insertExercise("running 200m","Run 200 meter as fast as you can.");
-        dbin.insertGroup("swimming");
-        dbin.insertGroupExercise("running 200m", "1");
-        dbin.insertGroupBelongsTo(1, 1);
-        dbin.insertIndoor(1, 4, 12);
-        dbin.insertOutdoor(1, 5, 12);
-        dbin.insertTemplateContains(1, "running 200m");
-        dbin.insertStrengthGoal("running 200m" , date_made, date_achieved, 23, 5, 3);
-        dbin.insertEnduranceGoal("running 200m", date_made, date_achieved, time, 1000);
-        dbin.insertStrengthResults("running 200m", 1, 3, 7, "a random note", 34, 12, 3);
-        dbin.insertEnduranceResults("running 200m", 1, 3, 7, "a random note", 34, 12);
-        dbin.insertSimilarExercise("running 200m", "running 200m");
+        dbin.insertGroup("running");
+        //dbin.insertGroupExercise("running 200m", "1");
+        //dbin.insertGroupBelongsTo(1, 1);
+        //dbin.insertIndoor(1, 4, 12);
+        //dbin.insertOutdoor(1, 5, 12);
+        //dbin.insertTemplateContains(1, "running 200m");
+        //dbin.insertStrengthGoal("running 200m" , date_made, date_achieved, 23, 5, 3);
+        //dbin.insertEnduranceGoal("running 200m", date_made, date_achieved, time, 1000);
+        //dbin.insertStrengthResults("running 200m", 1, 3, 7, "a random note", 34, 12, 3);
+        //dbin.insertEnduranceResults("running 200m", 1, 3, 7, "a random note", 34, 12);
+        //dbin.insertSimilarExercise("running 200m", "running 200m");
+
+        textUI textUI = new textUI();
+        textUI.app();
 
         /*try (Connection conn = DriverManager.getConnection(dbURL, username, password);){
             if (conn != null) {
